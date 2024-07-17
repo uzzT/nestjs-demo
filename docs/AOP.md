565f5ee75badd72b018d606552d7de472db0ff01
uzzt <uzzzt@outlook.com>
2024-07-17 11:57:43 +0800

    docs: AOP

---
+++ docs/AOP.md
@@ -0,0 +1,15 @@
+# AOP
+
+面向切面编程
+
+## 好处
+
+把通用逻辑分离到切面中，保持业务逻辑的的纯粹，切面逻辑可以复用，可以动态增删
+
+## nest 中实现 AOP 的方式
+
+- Middleware (中间件)
+- Interceptor
+- Guard
+- ExceptionFilter
+- Pipe
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Middleware
+  // app.use((req, res, next) => {
+  //   console.log(req, res, next);
+  //   next();
+  // });
   app.useStaticAssets('public', { prefix: '/static' });
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
